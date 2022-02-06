c89b3734c4a260eb71c2c21a9cca191de1516303
Daniel Evans <daniel.fred.evans@gmail.com>
2022-02-06 22:17:43 +0000

    Add files to map model

--- maps/models.py
+++ maps/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Map(models.Model):
-    map_id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     modified_date = models.DateField()
+    jpg_file = models.FileField()
+    yrd_file = models.FileField()
+    his_file = models.FileField()
--- maps/tests.py
+++ maps/tests.py
@@ -1,8 +1,17 @@
+from unittest import mock
+
+from django.core.files import File
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
 from .models import Map
+
+
+def mock_file(filename: str) -> mock.MagicMock:
+    mock_file = mock.MagicMock(spec=File, name="MockFile")
+    mock_file.name = filename
+    return mock_file
 
 
 # Create your tests here.
@@ -15,10 +24,25 @@ class MapsIndexViewTests(TestCase):
 
 class MapModelTests(TestCase):
     def test_map_id_1001_should_be_valid(self):
-        map = Map(map_id=1001, modified_date=timezone.now())
-        self.assertEqual(1001, map.map_id)
+        map = Map(id=1001)
+        self.assertEqual(1001, map.id)
 
     def test_modified_date_is_stored(self):
         mod_date = timezone.datetime(2020, 1, 1, 0, 0, 0)
-        map = Map(map_id=1001, modified_date=mod_date)
+        map = Map(modified_date=mod_date)
         self.assertEqual(mod_date, map.modified_date)
+
+    def test_jpg_file_is_stored(self):
+        jpg_file = mock_file("1001.jpg")
+        map = Map(jpg_file=jpg_file)
+        self.assertEqual("1001.jpg", map.jpg_file.name)
+
+    def test_yrd_file_is_stored(self):
+        yrd_file = mock_file("1001.yrd")
+        map = Map(yrd_file=yrd_file)
+        self.assertEqual("1001.yrd", map.yrd_file.name)
+
+    def test_his_file_is_stored(self):
+        his_file = mock_file("1001.his")
+        map = Map(his_file=his_file)
+        self.assertEqual("1001.his", map.his_file.name)
