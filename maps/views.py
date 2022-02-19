51d90d0ee4cb4d9d2c6973d91c85e73b2eb0cb6d
Daniel Evans <daniel.fred.evans@gmail.com>
2022-02-19 15:39:05 +0000

    Handle failure scenarios more gracefully

--- maps/views.py
+++ maps/views.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from typing import cast
 
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import get_object_or_404, redirect
-from django.utils.timezone import make_aware
 
 from .models import Map
 
@@ -36,12 +35,29 @@ def modified_date(request: HttpRequest, pk: int) -> JsonResponse:
 
 
 def update(request: HttpRequest, pk: int) -> HttpResponse:
-    modified_date = datetime.strptime(request.POST["modified_date"], r"%Y-%m-%d").date()
+    if "modified_date" not in request.POST:
+        return HttpResponseBadRequest("modified_date parameter missing")
+
+    try:
+        modified_date = datetime.strptime(
+            request.POST["modified_date"], r"%Y-%m-%d"
+        ).date()
+    except ValueError:
+        return HttpResponseBadRequest("Malformed modified_date, must be YYYY-MM-DD")
+
+    jpg_exists = "jpg_file" in request.FILES
+    yrd_exists = "yrd_file" in request.FILES
+    his_exists = "his_file" in request.FILES
 
     if Map.objects.filter(pk=pk).exists():
         map_obj = cast(Map, Map.objects.get(pk=pk))
         map_obj.modified_date = modified_date
     else:
+        if not (jpg_exists and yrd_exists and his_exists):
+            return HttpResponseBadRequest(
+                f"Map {pk} does not exist, all files must be provided"
+            )
+
         map_obj = cast(
             Map,
             Map.objects.create(
@@ -50,9 +66,14 @@ def update(request: HttpRequest, pk: int) -> HttpResponse:
             ),
         )
 
-    map_obj.jpg_file.save(f"{pk}.jpg", request.FILES["jpg_file"])
-    map_obj.yrd_file.save(f"{pk}.yrd", request.FILES["yrd_file"])
-    map_obj.his_file.save(f"{pk}.his", request.FILES["his_file"])
+    if jpg_exists:
+        map_obj.jpg_file.save(f"{pk}.jpg", request.FILES["jpg_file"])
+
+    if yrd_exists:
+        map_obj.yrd_file.save(f"{pk}.yrd", request.FILES["yrd_file"])
+
+    if his_exists:
+        map_obj.his_file.save(f"{pk}.his", request.FILES["his_file"])
 
     map_obj.save()
 
