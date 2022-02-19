860e4df71fc413cca0eebc2136341e5586b3f2b2
Daniel Evans <daniel.fred.evans@gmail.com>
2022-02-19 17:04:20 +0000

    Use file overwrite

--- fymserver/storage_backends.py
+++ fymserver/storage_backends.py
@@ -6,5 +6,5 @@ class MapDataS3Storage(S3Boto3Storage):
     bucket_name = "freightyardmanager-serverdata"
     location = "maps"
     default_acl = "private"
-    file_overwrite = False
+    file_overwrite = True
     custom_domain = False
