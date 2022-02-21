5faee85ebbb1f9f84b8884aaf1d77e7c36b44c5f
Daniel Evans <daniel.fred.evans@gmail.com>
2022-02-21 21:11:48 +0000

    Add train model admin

--- trains/admin.py
+++ trains/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Train
+
+
+# Register your models here.
+class TrainAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "from_player",
+                    "to_player",
+                    "downloaded_by",
+                    "upload_date",
+                    "state",
+                ]
+            },
+        ),
+        ("Files", {"fields": ["train_file"]}),
+    ]
+
+
+admin.site.register(Train, TrainAdmin)
