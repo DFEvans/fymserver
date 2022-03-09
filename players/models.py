0a91fe194535c71113701f5be1a396289d161543
Daniel Evans <daniel.fred.evans@gmail.com>
2022-03-09 22:11:57 +0000

    Add custom string representation of Player models

--- players/models.py
+++ players/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class Player(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"Player ({self.username})"
