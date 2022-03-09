b3dd75a6adeceda4d2c9242103cad6597b6876ec
Daniel Evans <daniel.fred.evans@gmail.com>
2022-03-09 21:28:54 +0000

    Add simple players view test

--- players/tests.py
+++ players/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class PlayerUuidViewTests(TestCase):
+    def test_players_uuid_returns_a_uuid(self):
+        url = reverse("players:uuid")
+        response = self.client.get(url, {"username": "Danny252"})
+
+        self.assertEqual(200, response.status_code)
+        assert "uuid" in str(response.content)
